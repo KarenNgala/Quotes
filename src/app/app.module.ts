b269fea5e336d24030f8bb899894b5bd2df9fa84
KarenNgala <ngalalalu@gmail.com>
2020-06-12 12:53:22 +0300

    Auto generate custom pipe to count passed time from posting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quotes/quote-details/quote-details.component';
+import { TimeCountPipe } from './time-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
