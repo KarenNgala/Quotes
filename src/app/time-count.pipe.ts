184d871aab0d7ca0e327bb6a8bd25ecf529247c4
KarenNgala <ngalalalu@gmail.com>
2020-06-12 16:04:55 +0300

    Add code to calculate time that has passed since posting a quote

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -5,8 +5,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): string {
+    const dayInSeconds = 86400;
+    let today:Date = new Date();
+    let todayWithoutTime:any = new Date (today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs(todayWithoutTime - value);
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var daysCount = dateDifferenceSeconds/dayInSeconds; // number of days
+    var days = `${daysCount} days ago`;
+    var monthsCount = Math.round(daysCount/30);
+    var months = `${monthsCount} months ago`;
+    var yearsCount = Math.round(monthsCount/12);
+    var years = `${yearsCount} years ago`;
+
+    if(value <= todayWithoutTime){
+      if(value = todayWithoutTime){
+        return `today`;
+      }else if (daysCount < 30){
+        return days;
+      }else if (daysCount >= 30){
+        if (monthsCount <= 12){
+          return months;
+        } else if(monthsCount > 12){
+          return years;
+        }
+      }
+    }else{
+      // Todo change this once form validation is in
+      return `on a future date`;
+    }
   }
 
 }
