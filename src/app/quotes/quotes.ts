ed34a41158d7a2f11cc45257858ac944f3ad89e9
KarenNgala <ngalalalu@gmail.com>
2020-06-12 11:45:16 +0300

    Change quote to text to avoid confussion

--- src/app/quotes/quotes.ts
+++ src/app/quotes/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     public showDetail:boolean;
-    constructor(public id:number, public quote:string, public author:string, public submitName:string, public postDate:Date){
+    constructor(public id:number, public text:string, public author:string, public submitName:string, public postDate:Date){
         this.showDetail=false;
     }
 }
