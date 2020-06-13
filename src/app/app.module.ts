9efd5a24bcf38a935a225e57f45638cd930a8bd8
KarenNgala <ngalalalu@gmail.com>
2020-06-13 13:22:27 +0300

    Initialize form component and minor text changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quotes/quote-details/quote-details.component';
 import { TimeCountPipe } from './time-count.pipe';
+import { UploadFormComponent } from './upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteDetailsComponent,
-    TimeCountPipe
+    TimeCountPipe,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
